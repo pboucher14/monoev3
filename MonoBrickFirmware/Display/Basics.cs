455697146820813200645bf2ecd16b56855d5fe6
Patrice Boucher <patrice_boucher@msn.com>
2021-01-15 14:55:00 -0500

    Added constructor for Rectangle Added method for DrawRectangle and WriteText Formatted files with CodeMaid

--- MonoBrickFirmware/Display/Basics.cs
+++ MonoBrickFirmware/Display/Basics.cs
@@ -4,56 +4,85 @@ namespace MonoBrickFirmware.Display
 {
 	public struct Point
 	{
+		#region Public Properties
+
 		public int X { get; set; }
 		public int Y { get; set; }
-		
-		public Point (int x, int y) : this()
+
+		#endregion
+
+		#region Public Constructors
+
+		public Point(int x, int y) : this()
 		{
 			this.X = x;
 			this.Y = y;
 		}
-		static public Point operator+(Point lhs, Point rhs)
-		{
-			return new Point(lhs.X + rhs.X, lhs.Y + rhs.Y);
-		}
-		
-		static public Point operator-(Point lhs, Point rhs)
+
+		#endregion
+
+		#region Public Methods
+
+		static public Point operator -(Point lhs, Point rhs)
 		{
 			return new Point(lhs.X - rhs.X, lhs.Y - rhs.Y);
 		}
-		
-		static public Point operator*(Point p, int factor)
+
+		static public Point operator *(Point p, int factor)
 		{
 			return new Point(p.X * factor, p.Y * factor);
 		}
-		
-		static public Point operator*(Point p, double factor)
-		{			
-			return new Point((int)Math.Round(p.X * factor), (int)Math.Round (p.Y * factor));
-		}				
+
+		static public Point operator *(Point p, double factor)
+		{
+			return new Point((int)Math.Round(p.X * factor), (int)Math.Round(p.Y * factor));
+		}
+
+		static public Point operator +(Point lhs, Point rhs)
+		{
+			return new Point(lhs.X + rhs.X, lhs.Y + rhs.Y);
+		}
+
+		#endregion
 	}
-	
+
 	public struct Rectangle
 	{
+		#region Public Properties
+
 		public Point P1 { get; set; }
 		public Point P2 { get; set; }
-		
+
+		#endregion
+
+		#region Public Constructors
+
 		public Rectangle(Point p1, Point p2) : this()
 		{
 			this.P1 = p1;
 			this.P2 = p2;
 		}
-		
-		static public Rectangle operator+(Rectangle r, Point p)
+
+		public Rectangle(int x1, int y1, int x2, int y2) : this()
 		{
-			return new Rectangle(r.P1 + p, r.P2 + p);
+			this.P1 = new Point(x1, y1);
+			this.P2 = new Point(x2, y2);
 		}
-		
-		static public Rectangle operator-(Rectangle r, Point p)
+
+		#endregion
+
+		#region Public Methods
+
+		static public Rectangle operator -(Rectangle r, Point p)
 		{
 			return new Rectangle(r.P1 - p, r.P2 - p);
 		}
-			
+
+		static public Rectangle operator +(Rectangle r, Point p)
+		{
+			return new Rectangle(r.P1 + p, r.P2 + p);
+		}
+
+		#endregion
 	}
 }
-
--- MonoBrickFirmware/Display/Lcd.cs
+++ MonoBrickFirmware/Display/Lcd.cs
@@ -1,145 +1,177 @@
-using System;
-using System.Runtime.CompilerServices;
 using MonoBrickFirmware.Tools;
-
 
 namespace MonoBrickFirmware.Display
 {
-  public static class Lcd
-  {
+	public static class Lcd
+	{
 
+		#region Public Enums
 
-	static Lcd()
-    {
-		if(PlatFormHelper.RunningPlatform == PlatFormHelper.Platform.EV3)
+		public enum Alignment { Left, Center, Right };
+
+		public enum ArrowOrientation { Left, Right, Down, Up }
+
+		#endregion
+
+		#region Public Properties
+
+		public static int Height { get { return Instance.Height; } }
+
+		public static int Width { get { return Instance.Width; } }
+
+		#endregion
+
+		#region Internal Properties
+
+		internal static ILcd Instance { get; set; }
+
+		#endregion
+
+		#region Public Constructors
+
+		static Lcd()
 		{
-			Instance = new EV3Lcd();
-			((EV3Lcd)Instance).Initialize();
+			if (PlatFormHelper.RunningPlatform == PlatFormHelper.Platform.EV3)
+			{
+				Instance = new EV3Lcd();
+				((EV3Lcd)Instance).Initialize();
+			}
+			else
+			{
+				Instance = null; //Not running on a EV3
+			}
 		}
-		else
+
+		#endregion
+
+		#region Public Methods
+
+		public static void Clear()
 		{
-			Instance = null; //Not running on a EV3
+			Instance.Clear();
 		}
-    }
-    public enum Alignment { Left, Center, Right };
-    public enum ArrowOrientation { Left, Right, Down, Up }
-	internal static ILcd Instance{ get; set; }
-	public static int Width { get{return Instance.Width;}}
-	public static int Height { get{return Instance.Height;}}
-    public static void SetPixel(int x, int y, bool color)
-    {
-      Instance.SetPixel(x,y,color);
-    }
 
-    public static bool IsPixelSet(int x, int y)
-    {
-      return Instance.IsPixelSet(x, y);
-    }
+		public static void ClearLines(int y, int count)
+		{
+			Instance.ClearLines(y, count);
+		}
 
-    public static void Update(int yOffset = 0)
-    {
-      Instance.Update(yOffset);
-    }
+		public static void DrawArrow(Rectangle r, Lcd.ArrowOrientation orientation, bool color)
+		{
+			Instance.DrawArrow(r, orientation, color);
+		}
 
-    public static void SaveScreen()
-    {
-      Instance.SaveScreen();
-    }
+		public static void DrawBitmap(Bitmap bm, Point p)
+		{
+			Instance.DrawBitmap(bm, p);
+		}
 
-    public static void LoadScreen()
-    {
-      Instance.LoadScreen();
-    }
+		public static void DrawBitmap(BitStreamer bs, Point p, uint xsize, uint ysize, bool color)
+		{
+			Instance.DrawBitmap(bs, p, xsize, ysize, color);
+		}
 
-    public static void ShowPicture(byte[] picture)
-    {
-      Instance.ShowPicture(picture);
-    }
+		public static void DrawCircle(Point center, ushort radius, bool color, bool fill)
+		{
+			Instance.DrawCircle(center, radius, color, fill);
+		}
 
-    public static void TakeScreenShot()
-    {
-      Instance.TakeScreenShot();
-    }
+		public static void DrawEllipse(Point center, ushort radiusA, ushort radiusB, bool color, bool fill)
+		{
+			Instance.DrawEllipse(center, radiusA, radiusB, color, fill);
+		}
 
-    public static void TakeScreenShot(string directory, string fileName)
-    {
-      Instance.TakeScreenShot(directory,fileName);
-    }
+		public static void DrawHLine(Point startPoint, int length, bool color)
+		{
+			Instance.DrawHLine(startPoint, length, color);
+		}
 
-    public static void ClearLines(int y, int count)
-    {
-      Instance.ClearLines(y, count);
-    }
+		public static void DrawLine(Point start, Point end, bool color)
+		{
+			Instance.DrawLine(start, end, color);
+		}
 
-    public static void Clear()
-    {
-      Instance.Clear();
-    }
+		public static void DrawRectangle(int x1, int y1, int x2, int y2, bool color, bool fill)
+		{
+			Instance.DrawRectangle(new Rectangle(x1, y1, x2, y2), color, fill);
+		}
 
-    public static void DrawVLine(Point startPoint, int height, bool color)
-    {
-      Instance.DrawVLine(startPoint,height,color);
-    }
+		public static void DrawRectangle(Rectangle r, bool color, bool fill)
+		{
+			Instance.DrawRectangle(r, color, fill);
+		}
 
-    public static void DrawHLine(Point startPoint, int length, bool color)
-    {
-      Instance.DrawHLine(startPoint,length,color);
-    }
+		public static void DrawVLine(Point startPoint, int height, bool color)
+		{
+			Instance.DrawVLine(startPoint, height, color);
+		}
 
-    public static void DrawBitmap(Bitmap bm, Point p)
-    {
-      Instance.DrawBitmap(bm,p);
-    }
+		public static bool IsPixelSet(int x, int y)
+		{
+			return Instance.IsPixelSet(x, y);
+		}
 
-    public static void DrawBitmap(BitStreamer bs, Point p, uint xsize, uint ysize, bool color)
-    {
-      Instance.DrawBitmap(bs,p,xsize,ysize,color);
-    }
+		public static void LoadScreen()
+		{
+			Instance.LoadScreen();
+		}
 
-    public static int TextWidth(Font f, string text)
-    {
-      return Instance.TextWidth(f, text);
-    }
+		public static void SaveScreen()
+		{
+			Instance.SaveScreen();
+		}
 
-    public static void WriteText(Font f, Point p, string text, bool color)
-    {
-      Instance.WriteText(f,p,text, color);
-    }
+		public static void SetPixel(int x, int y, bool color)
+		{
+			Instance.SetPixel(x, y, color);
+		}
 
-    public static void DrawArrow(Rectangle r, Lcd.ArrowOrientation orientation, bool color)
-    {
-      Instance.DrawArrow(r,orientation,color);
-    }
+		public static void ShowPicture(byte[] picture)
+		{
+			Instance.ShowPicture(picture);
+		}
 
-    public static void WriteTextBox(Font f, Rectangle r, string text, bool color)
-    {
-      Instance.WriteTextBox(f,r,text,color);
-    }
+		public static void TakeScreenShot()
+		{
+			Instance.TakeScreenShot();
+		}
 
-    public static void WriteTextBox(Font f, Rectangle r, string text, bool color, Lcd.Alignment aln)
-    {
-      Instance.WriteTextBox(f,r,text,color,aln);
-    }
+		public static void TakeScreenShot(string directory, string fileName)
+		{
+			Instance.TakeScreenShot(directory, fileName);
+		}
 
-    public static void DrawCircle(Point center, ushort radius, bool color, bool fill)
-    {
-      Instance.DrawCircle(center,radius,color,fill);
-    }
+		public static int TextWidth(Font f, string text)
+		{
+			return Instance.TextWidth(f, text);
+		}
 
-    public static void DrawEllipse(Point center, ushort radiusA, ushort radiusB, bool color, bool fill)
-    {
-      Instance.DrawEllipse(center,radiusA,radiusB,color,fill);
-    }
+		public static void Update(int yOffset = 0)
+		{
+			Instance.Update(yOffset);
+		}
 
-    public static void DrawLine(Point start, Point end, bool color)
-    {
-      Instance.DrawLine(start,end,color);
-    }
+		public static void WriteText(Font f, int x, int y, string text, bool color)
+		{
+			Instance.WriteText(f, new Point(x, y), text, color);
+		}
 
-    public static void DrawRectangle(Rectangle r, bool color, bool fill)
-    {
-      Instance.DrawRectangle(r,color,fill);
-    }
-  }
+		public static void WriteText(Font f, Point p, string text, bool color)
+		{
+			Instance.WriteText(f, p, text, color);
+		}
+
+		public static void WriteTextBox(Font f, Rectangle r, string text, bool color)
+		{
+			Instance.WriteTextBox(f, r, text, color);
+		}
+
+		public static void WriteTextBox(Font f, Rectangle r, string text, bool color, Lcd.Alignment aln)
+		{
+			Instance.WriteTextBox(f, r, text, color, aln);
+		}
+
+		#endregion
+
+	}
 }
-
